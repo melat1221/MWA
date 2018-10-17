72334bb8f923f005b206ad6abf594c0d3d481620
melathadgu <abemel26@gmail.com>
2018-10-16 19:07:01 -0500

    1

--- lab1/removeDuplicates/withPromises.js
+++ lab1/removeDuplicates/withPromises.js
@@ -1,33 +1,30 @@
 
-let x=[5,6,5,4,3,4,2,1,1]
+let x = [5, 6, 5, 4, 3, 4, 2, 1, 1]
 
+function removeDuplicates(a) {
+    const m = a.reduce(function (b, e, i) {
+        if (!(b.includes(e)))
+            b.push(e);
+        return b;
 
-function removeDuplicates(a){
-    
-     const m=a.reduce(function(b,e,i){
-if(!(b.includes(e)))
-b.push(e);
-return b;
+    }, []);
 
-},[]);
-    
     return m;
-    
+
 }
-const promis=function(arr){
-      
-      return new Promise(function(resolve,reject){
-          const result=removeDuplicates(arr);
-          if(result)
-             resolve(result);
-          else
-              reject(new Error());
-      });
-    
+const promis = function (arr) {
+
+    return new Promise(function (resolve, reject) {
+        const result = removeDuplicates(arr);
+        if (result)
+            resolve(result);
+        else
+            reject(new Error());
+    });
 }
 
 promis(x)
-    .then(result=>console.log(`result from promises: ${result}` ))
-    .catch(err=>console.error(err));
+    .then(result => console.log(`result from promises: ${result}`))
+    .catch(err => console.error(err));
 
 console.log("removing duplicates in progress");
